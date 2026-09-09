bff84e2a3a93dae9d4eab6c67db25ae0d4957cf4
Yan Nunes <yrn@cesar.school>
2026-09-09 12:03:58 -0300

    salva o historico ao finalizar a simulacao

--- src/simulation.c
+++ src/simulation.c
@@ -98,59 +98,6 @@ void criar_instancias(Simulacao *sim, int tempo) {
     }
 }
 
-int salvar_saida(
-    const Simulacao *sim,
-    Algoritmo algoritmo,
-    const char *nome_saida,
-    const Historico *hist
-) {
-    FILE *saida = fopen(nome_saida, "w");
-    int i;
-
-    if (!saida) {
-        fprintf(stderr,"Erro: nao foi possivel criar '%s'.\n",nome_saida);
-        return 0;
-    }
-
-    fprintf(saida,"EXECUTION BY %s\n",algoritmo == RATE ? "RATE" : "EDF");
-
-    for (i = 0; i < hist->qtd_trechos; i++) {
-        const Trecho *trecho = &hist->trechos[i];
-        int duracao = trecho->fim - trecho->inicio;
-
-        if (trecho->tarefa == -1) {
-            fprintf(saida,"idle for %d units\n",duracao);
-        } 
-        else {
-            fprintf(saida,"[%s] for %d units - %c\n",sim->tarefas[trecho->tarefa].nome,duracao,trecho->motivo);
-        }
-    }
-
-    fprintf(saida, "LOST DEADLINES\n");
-
-    for (i = 0; i < sim->quantidade; i++) {
-        fprintf(saida,"[%s] %d\n",sim->tarefas[i].nome,sim->tarefas[i].perdidas);
-    }
-
-    fprintf(saida, "COMPLETE EXECUTION\n");
-
-    for (i = 0; i < sim->quantidade; i++) {
-        fprintf(saida,"[%s] %d\n",sim->tarefas[i].nome,sim->tarefas[i].concluidas);
-    }
-
-    fprintf(saida, "KILLED\n");
-
-    for (i = 0; i < sim->quantidade; i++) {
-        fprintf(saida,"[%s] %d\n",sim->tarefas[i].nome,sim->tarefas[i].encerradas);
-    }
-    if (fclose(saida)) {
-        fprintf(stderr,"Erro ao finalizar '%s'.\n",nome_saida);
-    return 0;
-    }
-
-return 1;
-}
-
 int executar(
     Simulacao *sim,
     Algoritmo algoritmo,
@@ -158,6 +105,7 @@ int executar(
 ) {
     Historico hist = {0};
     int erro_memoria = 0;
+    int resultado = 0;
     int tempo;
 
     for (tempo = 0;
@@ -195,13 +143,12 @@ int executar(
 
     if (erro_memoria) {
         fprintf(stderr,"Erro: memoria insuficiente durante a simulacao.\n");
-        free(hist.trechos);
-        return 0;
+    } 
+    else {
+        resultado = salvar_saida(sim,algoritmo,nome_saida,&hist);
     }
 
     free(hist.trechos);
 
-    (void) nome_saida;
-
-    return 1;
+    return resultado;
 }
